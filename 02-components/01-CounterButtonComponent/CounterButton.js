3dd45ce33dfc91800b970772465f9258f8e27f6d
javascriptru <67475495+jsru-1@users.noreply.github.com>
2023-05-30 19:29:42 +0300

    Added task Компонент CounterButton

---
+++ 02-components/01-CounterButtonComponent/CounterButton.js
@@ -0,0 +1,9 @@
+import { defineComponent } from './vendor/vue.esm-browser.js';
+
+export default defineComponent({
+  name: 'CounterButton',
+
+  // Компонент должен иметь входной параметр и порождать событие
+
+  template: `<button type="button">1</button>`,
+});
