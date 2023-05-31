165fc0dda06e7f7ccbd050b08ed0552ae0a03966
javascriptru <67475495+jsru-1@users.noreply.github.com>
2023-06-01 00:44:34 +0300

    Added teacher solution for task: MeetupCover

---
+++ 02-components/03-MeetupCover/components/MeetupCover.js
@@ -0,0 +1,20 @@
+import { defineComponent } from '../vendor/vue.esm-browser.js';
+
+export default defineComponent({
+  name: 'MeetupCover',
+
+  props: {
+    title: {
+      type: String,
+    },
+
+    image: {
+      type: String,
+    },
+  },
+
+  template: `
+    <div class="meetup-cover" :style="image && { '--bg-url': \`url('\${image}')\` }">
+      <h1 class="meetup-cover__title">{{ title }}</h1>
+    </div>`,
+});
