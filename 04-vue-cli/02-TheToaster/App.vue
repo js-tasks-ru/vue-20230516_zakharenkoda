16f5a67dd35fb70687bf616d21e5a2457f1fdb24
javascriptru <67475495+jsru-1@users.noreply.github.com>
2023-06-09 15:38:36 +0300

    Added task TheToaster

---
+++ 04-vue-cli/02-TheToaster/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <TheToaster ref="toaster" />
+  <p>
+    <button type="button" @click="handleSuccessClick">Add Success Message</button>
+    <button type="button" @click="handleErrorClick">Add Error Message</button>
+  </p>
+</template>
+
+<script>
+import TheToaster from './components/TheToaster.vue';
+
+export default {
+  name: 'App',
+
+  components: { TheToaster },
+
+  methods: {
+    handleSuccessClick() {
+      this.$refs.toaster.success('Success ' + new Date().toLocaleTimeString());
+    },
+
+    handleErrorClick() {
+      this.$refs.toaster.error('Error ' + new Date().toLocaleTimeString());
+    },
+  },
+};
+</script>
---
+++ 04-vue-cli/02-TheToaster/components/TheToaster.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="toasts">
+    <div class="toast toast_success">
+      <UiIcon class="toast__icon" icon="check-circle" />
+      <span>Success Toast Example</span>
+    </div>
+
+    <div class="toast toast_error">
+      <UiIcon class="toast__icon" icon="alert-circle" />
+      <span>Error Toast Example</span>
+    </div>
+  </div>
+</template>
+
+<script>
+import UiIcon from './UiIcon.vue';
+
+export default {
+  name: 'TheToaster',
+
+  components: { UiIcon },
+};
+</script>
+
+<style scoped>
+.toasts {
+  position: fixed;
+  bottom: 8px;
+  right: 8px;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  white-space: pre-wrap;
+  z-index: 999;
+}
+
+@media all and (min-width: 992px) {
+  .toasts {
+    bottom: 72px;
+    right: 112px;
+  }
+}
+
+.toast {
+  display: flex;
+  flex: 0 0 auto;
+  flex-direction: row;
+  align-items: center;
+  padding: 16px;
+  background: #ffffff;
+  box-shadow: 0 2px 6px rgba(0, 0, 0, 0.15);
+  border-radius: 4px;
+  font-size: 18px;
+  line-height: 28px;
+  width: auto;
+}
+
+.toast + .toast {
+  margin-top: 20px;
+}
+
+.toast__icon {
+  margin-right: 12px;
+}
+
+.toast.toast_success {
+  color: var(--green);
+}
+
+.toast.toast_error {
+  color: var(--red);
+}
+</style>
