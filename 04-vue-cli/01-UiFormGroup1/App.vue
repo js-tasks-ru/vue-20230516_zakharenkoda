15b73064135c7a4e8390262a9fafe9930fa2ccf1
javascriptru <67475495+jsru-1@users.noreply.github.com>
2023-06-09 15:38:23 +0300

    Added task UiFormGroup1

---
+++ 04-vue-cli/01-UiFormGroup1/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <UiContainer class="sample">
+    <UiFormGroup>
+      <div class="input-group">
+        <input class="form-control" placeholder="No label" />
+      </div>
+    </UiFormGroup>
+
+    <hr />
+
+    <UiFormGroup label="Label Text">
+      <div class="input-group">
+        <input class="form-control" placeholder="With label" />
+      </div>
+    </UiFormGroup>
+
+    <hr />
+
+    <div>
+      <UiFormGroup inline>
+        <div class="input-group">
+          <input class="form-control" placeholder="Inline" />
+        </div>
+      </UiFormGroup>
+
+      <UiFormGroup inline>
+        <div class="input-group">
+          <input class="form-control" placeholder="Inline" />
+        </div>
+      </UiFormGroup>
+    </div>
+  </UiContainer>
+</template>
+
+<script>
+import UiFormGroup from './components/UiFormGroup.vue';
+import UiContainer from './components/UiContainer.vue';
+
+export default {
+  name: 'App',
+
+  components: { UiContainer, UiFormGroup },
+};
+</script>
+
+<style scoped>
+hr {
+  margin: 1rem auto;
+}
+</style>
---
+++ 04-vue-cli/01-UiFormGroup1/components/UiFormGroup.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="form-group">
+    <!-- form-group_inline -->
+    <label class="form-group__label">label text</label>
+    <!-- CONTENT -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'UiFormGroup',
+};
+</script>
+
+<style scoped>
+.form-group {
+  position: relative;
+  margin-bottom: 24px;
+}
+
+.form-group.form-group_inline {
+  display: inline-block;
+  margin-bottom: 0;
+}
+
+.form-group.form-group_inline + .form-group.form-group_inline {
+  margin-left: 16px;
+}
+
+.form-group__label {
+  font-weight: 400;
+  font-size: 20px;
+  line-height: 28px;
+  color: var(--body-color);
+  margin-bottom: 10px;
+  display: block;
+}
+</style>
